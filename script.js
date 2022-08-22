ed90b1476cff0ed06ea0f26e87e4c5ebb96bc3c1
Ahmed Awadelkareem <ahmdhelps@gmail.com>
2022-08-22 19:05:08 +0100

    grid shows up but it does not take up all the space

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 for (let i = 0; i < 16; i++) {
 	let newRow = document.createElement('div');
 	let container = document.querySelector('div#container');
+	newRow.classList.add('rows');
 	container.appendChild(newRow);
 }
 
 function createRow() {
 	for (let i = 0; i < 16; i++) {
 		let newDiv = document.createElement('div');
-		let container = document.querySelector('div.containers');
+		let container = document.querySelector('div.rows');
 		container.appendChild(newDiv);
 	}
 }
+
