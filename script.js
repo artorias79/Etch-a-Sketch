a53361696af98671bde0d84039a53990eeea886d
Ahmed Awadelkareem <ahmdhelps@gmail.com>
2022-08-22 19:58:39 +0100

    prompt for button not alerting if length or width is more than 100

--- script.js
+++ script.js
@@ -5,11 +5,27 @@ for (let i = 0; i < 16; i++) {
 	container.appendChild(newRow);
 }
 
-function createRow() {
+const rows = document.querySelectorAll('div.rows');
+
+rows.forEach(row => {
 	for (let i = 0; i < 16; i++) {
 		let newDiv = document.createElement('div');
-		let container = document.querySelector('div.rows');
-		container.appendChild(newDiv);
-	}
-}
+		newDiv.classList.add('squares');
+		row.appendChild(newDiv);
+	}	
+});
 
+const button = document.querySelector('button');
+button.addEventListener('click', function () {
+	let length = prompt('Select the amount of squares for the length of the grid: ');
+	let width = prompt('Select the amount of squares for the width of the grid: ');
+	if (typeof length != 'number' || typeof width != 'number') {
+		console.log(length);
+		return;
+	} else {
+		console.log(length);
+		if (length > 100 || width > 100) {
+			alert('Make sure each side is less than 100 squares');
+		} 
+	}
+})
