f754647d27044a0e545d6f207fda4bfc09d2db90
Ahmed Awadelkareem <ahmdhelps@gmail.com>
2022-08-23 20:14:52 +0100

    make it so that button asks for length and width and applies it

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+let container = document.querySelector('div#container');
+
 for (let i = 0; i < 16; i++) {
 	let newRow = document.createElement('div');
-	let container = document.querySelector('div#container');
 	newRow.classList.add('rows');
 	container.appendChild(newRow);
 }
 
 const rows = document.querySelectorAll('div.rows');
-
 rows.forEach(row => {
 	for (let i = 0; i < 16; i++) {
 		let newDiv = document.createElement('div');
@@ -19,13 +19,29 @@ const button = document.querySelector('button');
 button.addEventListener('click', function () {
 	let length = prompt('Select the amount of squares for the length of the grid: ');
 	let width = prompt('Select the amount of squares for the width of the grid: ');
-	if (typeof length != 'number' || typeof width != 'number') {
-		console.log(length);
-		return;
-	} else {
-		console.log(length);
-		if (length > 100 || width > 100) {
-			alert('Make sure each side is less than 100 squares');
-		} 
+	if (container.hasChildNodes()) {
+		container.removeChild(container.children[0]);
 	}
+	createColumns(length);
+	createRows(width);
+
 })
+
+function createColumns(length) {
+	for (let i = 0; i < length; i++) {
+		let newRow = document.createElement('div');
+		let container = document.querySelector('div#container');
+		newRow.classList.add('rows');
+		container.appendChild(newRow);
+	}
+}
+
+function createRows(width) {
+	rows.forEach(row => {
+		for (let i = 0; i < width; i++) {
+			let newDiv = document.createElement('div');
+			newDiv.classList.add('squares');
+			row.appendChild(newDiv);
+		}
+	});
+}
